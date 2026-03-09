3a99861c6cee07c07662c9d371f8190c28aad5c6
d27pomaramirez-lab <d27poma.ramirez@gmail.com>
2026-03-09 17:54:58 -0500

    feat: implementación de autenticación con JWT con filter chain

--- src/main/java/com/alura/forohub/infra/security/TokenService.java
+++ src/main/java/com/alura/forohub/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.alura.forohub.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
@@ -27,6 +28,22 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error al generar el token JWT", exception);
+        }
+    }
+
+    public String getSubject(String token) {
+        if (token == null) {
+            throw new RuntimeException("El token es nulo.");
+        }
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.require(algorithm)
+                    .withIssuer("Foro Hub")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido o expirado!", exception);
         }
     }
 
